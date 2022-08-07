013f2dec448eab75b6a600d17271c7ecc6867400
Abhay Bhatia <abhay1732002@gmail.com>
2022-08-07 16:58:20 +0530

    added start and port

--- app.js
+++ app.js
@@ -173,6 +173,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(3000,()=>{
-    console.log('http://localhost:3000/')
+const port = process.env.PORT ||3000
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`)
 })
